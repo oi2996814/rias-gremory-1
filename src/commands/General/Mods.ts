85617f6dc26ba7f686e6dd8abc1b5ac3481838e1
00300 <90952986+well300@users.noreply.github.com>
2021-12-26 17:01:43 +0530

    🎅🏻

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -20,7 +20,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const kacchan =
-			"https://c.tenor.com/H2tVYsdRw0kAAAPo/zerotwo-darlinginthefranxx.mp4";
+			"https://c.tenor.com/63woshESSMMAAAPo/high-school-dxd-rias-gremory.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: kacchan },
@@ -28,21 +28,9 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `🏮 ᴍᴏᴅᴇʀᴀᴛᴏʀꜱ\n
+				caption: `🍒 ᴍᴏᴅᴇʀᴀᴛᴏʀ\n
 ╰• ᴡᴇʟʟ300
 🎗ᴄᴏɴᴛᴀᴄᴛ: wa.me/917842346461?text=𝙝𝙚𝙡𝙡𝙤+300/>
-──────────────
-╰• ᴘᴀɪɴ
-🎗ᴄᴏɴᴛᴀᴄᴛ: wa.me/919662713165?text=𝙝𝙚𝙡𝙡𝙤+ᴘᴀɪɴ
-──────────────
-╰• ʟᴏʀᴅ ʀᴀᴋᴇꜱʜ
-🎗ᴄᴏɴᴛᴀᴄᴛ: wa.me/918473956301?text=𝙝𝙚𝙡𝙡𝙤+ʀᴀᴋᴇꜱʜ
-──────────────
-╰• ʙʟᴀᴢᴇ
-🎗ᴄᴏɴᴛᴀᴄᴛ: wa.me/917721059975?text=𝙝𝙚𝙡𝙡𝙤+ʙʟᴀᴢᴇ
-──────────────────
-🅂🅄🄿🄿🄾🅁🅃 🄶🅁🄾🅄🄿                                        
-🎗 https://chat.whatsapp.com/JboXSiHnBLBLE9cJ5Ai8ur 
 •━━━ ✽ • ✽ ━━━•`,
 			}
 		);
